d2b0178fb7ea56f0e0657c2f1f85175c8ab7c927
Your Name <jasonbataillon@gmail.com>
2025-02-13 11:56:25 -0500

    Changed watchlist to get stockname from the frontend

--- api/watchlist.js
+++ api/watchlist.js
@@ -4,14 +4,17 @@ const prisma = require('../prisma');
 const { authenticate } = require('./auth');
 
 router.post('/add', authenticate, async (req, res, next) => {
-  const { stockTicker } = req.body;
+  const { stockTicker, stockName } = req.body;
   const userId = req.user.id;
 
   try {
     const stock = await prisma.stock.upsert({
       where: { symbol: stockTicker },
       update: {},
-      create: { symbol: stockTicker },
+      create: {
+        symbol: stockTicker,
+        name: stockName, // Use the provided stock name
+      },
     });
 
     const watchlist = await prisma.watchlist.create({
